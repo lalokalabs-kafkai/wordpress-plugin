dadedeba0b222011bf858dc664c5075a0b7921dd
Akshit Sethi <akshitsethi@gmail.com>
2020-10-27 14:51:04 +0530

    Polished the AJAX fetch process

--- src/assets/admin/js/admin.js
+++ src/assets/admin/js/admin.js
@@ -25,7 +25,7 @@
   $('.import-article').on('click', function(e) {
     e.preventDefault();
 
-    var article_id, table, parent_tr;
+    var article_id, table, parent_tr, first_td;
 
     // Article ID for fetching specific article
     article_id = $(this).data('id');
@@ -33,43 +33,47 @@
     // Main table
     table = $('.wp-list-table');
 
-    // Find parent tr
+    // Find parent tr and td
     parent_tr = $(this).parent().parent();
-
-    // Add overlay to table
-    parent_tr.find('.column-image').append('<div class="' + kafkaiwp_admin_l10n.prefix + 'loading"><span class="spinner is-active"></span></div>');
-    return;
+    first_td = parent_tr.find('.column-image');
 
     // Fetch info via an AJAX call
-    // $.ajax({
-    //   type: 'get',
-    //   url: ajaxurl,
-    //   data: {
-    //     action: kafkaiwp_admin_l10n.prefix + 'fetch_article',
-    //     article_id: '3101095a-fcff-4c3d-8e1a-bec82aba271',
-    //     // article_id: article_id,
-    //     _nonce: kafkaiwp_admin_l10n.nonce
-    //   },
-    //   error: function(xhr, status, error) {
-    //     if(status === 'error') {
-    //       $('#inline-article-container .error-response').html(kafkaiwp_admin_l10n.error_text);
-    //     }
-    //   }
-    // }).done(function(data) {
-    //   if(data.code === 'success') {
-    //     console.log(data.response);
-    //     var article_body = data.response.body;
+    $.ajax({
+      type: 'get',
+      url: ajaxurl,
+      data: {
+        action: kafkaiwp_admin_l10n.prefix + 'fetch_article',
+        article_id: article_id,
+        _nonce: kafkaiwp_admin_l10n.nonce
+      },
+      beforeSend: function() {
+        table.append('<div class="' + kafkaiwp_admin_l10n.prefix + 'ajax_overlay"></div>');
+        first_td.append('<div class="' + kafkaiwp_admin_l10n.prefix + 'loading"><span class="spinner is-active"></span></div>');
+      },
+      error: function(xhr, status, error) {
+        if(status === 'error') {
+          $('#inline-article-container .error-response').html(kafkaiwp_admin_l10n.error_text);
+        }
+      }
+    }).done(function(data) {
+      // Remove added elements for blocker
+      table.find('.' + kafkaiwp_admin_l10n.prefix + 'ajax_overlay').remove();
+      first_td.find('.' + kafkaiwp_admin_l10n.prefix + 'loading').remove();
 
-    //     // Turn line breaks into <br> and add HTML to article body
-    //     article_body = article_body.replace(/(?:\r\n|\r|\n)/g, '<br>');
-    //     $('#inline-article-container .article-body').html(article_body);
+      if(data.code === 'success') {
+        console.log(data.response);
+        var article_body = data.response.body;
 
-    //     // Open lightbox
-    //     tb_show(kafkaiwp_admin_l10n.window_title, '#TB_inline?height=604&width=772&inlineId=inline-article-container&modal=true');
-    //   } else {
-    //     notification(data.error, 'error');
-    //   }
-    // });
+        // Turn line breaks into <br> and add HTML to article body
+        article_body = article_body.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        $('#inline-article-container .article-body').html(article_body);
+
+        // Open lightbox
+        tb_show(kafkaiwp_admin_l10n.window_title, '#TB_inline?height=604&width=772&inlineId=inline-article-container&modal=true');
+      } else {
+        notification(data.error, 'error');
+      }
+    });
   });
 
   // Close modal
